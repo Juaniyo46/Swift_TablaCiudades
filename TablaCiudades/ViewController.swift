58f52fb1952998988088c8945a8aba23db47251d
Juan Antonio <juan.alvarez@escuelaestech.es>
2019-11-27 17:58:50 +0100

    Initial Commit

---
+++ TablaCiudades/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TablaCiudades
+//
+//  Created by juan.alvarez on 27/11/2019.
+//  Copyright © 2019 Juan Alvarez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
